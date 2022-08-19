ab6988baced40cde1caf6f07f9c08c0a33d185ba
Villalba8 <107590858+Villalba8@users.noreply.github.com>
2022-08-18 21:01:38 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+const input = 'patience is a virtue';
 
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+const resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+    const letter = input[i].toLowerCase();
+
+    if (letter === 'e' || letter === 'u') {
+        resultArray.push(letter);
+    }
+    
+    for (let j = 0; j < vowels.length; j++) {
+        const vowel = vowels[j];
+
+        if (letter === vowel) {
+        resultArray.push(letter);
+      }    
+    }
+ }
+ const resultString = resultArray.join('').toUpperCase();
+
+console.log(resultString);
